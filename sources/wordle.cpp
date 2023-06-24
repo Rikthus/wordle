a490d6eac1e7a58807ed0ce39e7bedee3d2fbf69
maxperei <maxperei@student.42lyon.fr>
2023-06-24 04:08:22 +0200

    Move prepocessing operations to header file in includes folder

--- sources/wordle.cpp
+++ sources/wordle.cpp
@@ -1,19 +1,4 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <algorithm>
-#include <random>
-#include <sys/stat.h>
-
-#define MAX_DICT_SIZE 100000
-#define MIN_DICT_SIZE 1000
-
-#define GREEN "\033[1;32m"
-#define YELLOW "\033[1;33m"
-#define GREY "\033[1;30m"
-#define END "\033[0m"
-
-using namespace std;
+#include "../includes/wordle.hpp"
 
 static	void	printWelcome(void)
 {
